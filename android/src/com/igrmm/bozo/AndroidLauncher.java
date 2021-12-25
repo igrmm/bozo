d309b2e3d557bca8d8918317310c71eae6d8f066
igrmm <igrmmdev@gmail.com>
2021-12-25 19:32:31 -0400

    add project created with libgdx wizard

---
+++ android/src/com/igrmm/bozo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.igrmm.bozo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.igrmm.bozo.Bozo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Bozo(), config);
+	}
+}
