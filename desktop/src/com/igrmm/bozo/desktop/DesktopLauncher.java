9c55aad35a28b15073185695c1210a0831291d51
igrmm <igrmmdev@gmail.com>
2021-12-25 21:24:59 -0400

    update gradle plugin and set lwjgl3 backend

--- desktop/src/com/igrmm/bozo/desktop/DesktopLauncher.java
+++ desktop/src/com/igrmm/bozo/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.igrmm.bozo.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.igrmm.bozo.Bozo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Bozo(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Bozo(), config);
 	}
 }
