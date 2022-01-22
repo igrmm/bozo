a6ff3e0d961e53be942fca73feb70ef03740c0de
igrmm <igrmmdev@gmail.com>
2022-01-22 16:07:34 -0400

    indent with tab

--- core/src/com/igrmm/bozo/GameScreen.java
+++ core/src/com/igrmm/bozo/GameScreen.java
@@ -6,26 +6,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen extends ScreenAdapter {
-    private final Bozo game;
-    private final SpriteBatch batch;
-    private final Texture img;
+	private final Bozo game;
+	private final SpriteBatch batch;
+	private final Texture img;
 
-    public GameScreen(Bozo game) {
-        this.game = game;
-        batch = new SpriteBatch();
-        img = game.assets.get(ImageAsset.PLAYER_IMG.path);
-    }
+	public GameScreen(Bozo game) {
+		this.game = game;
+		batch = new SpriteBatch();
+		img = game.assets.get(ImageAsset.PLAYER_IMG.path);
+	}
 
-    @Override
-    public void render(float delta) {
-        ScreenUtils.clear(1.0f, 1.0f, 0.66f, 1.0f);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
-    }
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(1.0f, 1.0f, 0.66f, 1.0f);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
 
-    @Override
-    public void hide() {
-        batch.dispose();
-    }
+	@Override
+	public void hide() {
+		batch.dispose();
+	}
 }
--- core/src/com/igrmm/bozo/ImageAsset.java
+++ core/src/com/igrmm/bozo/ImageAsset.java
@@ -1,11 +1,11 @@
 package com.igrmm.bozo;
 
 public enum ImageAsset {
-    PLAYER_IMG("images/player.png");
+	PLAYER_IMG("images/player.png");
 
-    public final String path;
+	public final String path;
 
-    ImageAsset(String path) {
-        this.path = path;
-    }
+	ImageAsset(String path) {
+		this.path = path;
+	}
 }
