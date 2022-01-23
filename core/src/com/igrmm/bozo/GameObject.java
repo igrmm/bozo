ec46ebc69127fbbab806aa0efb3d9a05f51114a9
igrmm <igrmmdev@gmail.com>
2022-01-23 16:20:25 -0400

    add GameObject class

---
+++ core/src/com/igrmm/bozo/GameObject.java
@@ -0,0 +1,14 @@
+package com.igrmm.bozo;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class GameObject {
+	public final Rectangle rectangle = new Rectangle();
+
+	public boolean overlaps(Rectangle r) {
+		return this.rectangle.overlaps(r);
+	}
+
+	public abstract void render(SpriteBatch batch);
+}
